e94da3d9b9bb18fcbb9f4ad9d056ae602496173e
powerhouse <rautnikhil373@gmail.com>
2026-03-19 05:58:17 +0000

    Settings: Allow limiting minimal % for auto brightness [1/2]

--- src/com/android/settings/display/AutoBrightnessSettings.java
+++ src/com/android/settings/display/AutoBrightnessSettings.java
@@ -17,17 +17,31 @@
 package com.android.settings.display;
 
 import android.app.settings.SettingsEnums;
+import android.content.Context;
 import android.os.Bundle;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.MinAutoBrightnessPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.search.SearchIndexable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class AutoBrightnessSettings extends DashboardFragment {
 
     private static final String TAG = "AutoBrightnessSettings";
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new MinAutoBrightnessPreferenceController(context));
+        return controllers;
+    }
 
     @Override
     public void onCreate(Bundle icicle) {
